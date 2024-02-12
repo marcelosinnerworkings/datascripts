e1d7cdb993c611035d3a0e754d75abe0c23f7002
Marcelo de Lima <marcelo.delima.brazil@gmail.com>
2024-02-12 15:22:27 -0300

    Refactor to include .env variables and simplify the connection

--- postgres_table_to_csv/postgres_table_to_csv.py
+++ postgres_table_to_csv/postgres_table_to_csv.py
@@ -24,18 +24,22 @@ are.
 
 import sqlalchemy
 import pandas as pd
-from decouple import config
+from dotenv import load_dotenv
+import os
 
 # Load environment variables from the .env file
-db_host = config('DB_HOST')
-db_name = config('DB_NAME')
-db_user = config('DB_USER')
-db_password = config('DB_PASSWORD')
-db_port = config('DB_PORT')
-db_table_name = config('DB_TABLE_NAME')
+load_dotenv()
+
+# Database environment variables
+# Remember to change the table name accordingly
+db_host = os.getenv('DB_HOST')
+db_name = os.getenv('DB_NAME')
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_port = os.getenv('DB_PORT')
+db_table_name = os.getenv('DB_TABLE_NAME')
 
 # Database connection parameters
-# Replace the data and insert those from your specific connection
 db_params = {
     'database': db_name,
     'user': db_user,
@@ -44,7 +48,6 @@ db_params = {
     'port': db_port
 }
 
-table_name = db_table_name
 csv_file_name = 'csv_cutie_file.csv'
 
 try:
@@ -54,11 +57,12 @@ try:
         f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['database']}"
     )
 
-    df = pd.read_sql(f'SELECT * FROM "public"."{table_name}"', engine)
+    df = pd.read_sql(f'SELECT * FROM "public"."{db_table_name}"', engine)
 
     df.to_csv(csv_file_name, index=False)
 
-    print(f'Data from table {table_name} has been exported to {csv_file_name}')
+    print(f'Data from table {db_table_name} has been exported to {csv_file_name}')
 
 except Exception as e:
     print(f'Error: {e}')
+
